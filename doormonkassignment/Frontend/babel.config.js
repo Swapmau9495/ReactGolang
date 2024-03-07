e62a1ed5cf92ef620c897427d2732d16288c27b2
Swapmau9495 <maneswap94@gmail.com>
2024-03-07 22:23:10 +0530

    Updated babel and jest configration

--- doormonkassignment/Frontend/babel.config.js
+++ doormonkassignment/Frontend/babel.config.js
@@ -1,4 +1,2 @@
-module.exports = {
-  presets: ["@babel/preset-env", "@babel/preset-react"],
-  plugins: ["@babel/plugin-transform-runtime"],
-};
+export const presets = ["@babel/preset-env", "@babel/preset-react"];
+export const plugins = ["@babel/plugin-transform-runtime"];
